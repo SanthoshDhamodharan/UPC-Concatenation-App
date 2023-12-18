defe6deb6fd80d7454f5be113f9da23322538671
Santhosh S D <139611473+SanthoshDhamodharan@users.noreply.github.com>
2023-12-18 19:42:48 +0530

    Update UPC_CONCATENATE_APP.py

--- UPC_CONCATENATE_APP.py
+++ UPC_CONCATENATE_APP.py
@@ -19,6 +19,13 @@ def preprocess_data(df):
     new_df = df_unique.groupby('offer_id2')['_14_char_barcode'].apply(lambda x: ','.join(x)).reset_index()
     return new_df
 
+# Function to create a download link for a file
+def get_binary_file_downloader_html(file_path, file_label):
+    with open(file_path, "rb") as f:
+        data = f.read()
+    b64 = base64.b64encode(data).decode()
+    return f'<a href="data:application/octet-stream;base64,{b64}" download="{file_label}">Click here to download {file_label}</a>'
+
 # Streamlit app
 st.title('UPC Concatenation App')
 
@@ -27,7 +34,7 @@ uploaded_file = st.file_uploader("Upload Excel File", type=["xlsx", "xls"])
 
 # Placeholder for user-specified file name
 file_name_placeholder = st.empty()
-file_name = file_name_placeholder.text_input("Enter the desired file name (without extension):", "processed_data")
+file_name = file_name_placeholder.text_input("Enter the desired file name (without extension):")
 
 # Button to start processing
 if st.button("Process Data"):
@@ -59,14 +66,6 @@ if st.button("Process Data"):
                 st.markdown(get_binary_file_downloader_html(temp_file_path, f"{file_name.strip()}.xlsx"), unsafe_allow_html=True)
         except Exception as e:
             st.error(f"An error occurred: {str(e)}")
-
-
-# Function to create a download link for a file
-def get_binary_file_downloader_html(file_path, file_label):
-    with open(file_path, "rb") as f:
-        data = f.read()
-    b64 = base64.b64encode(data).decode()
-    return f'<a href="data:application/octet-stream;base64,{b64}" download="{file_label}">Click here to download {file_label}</a>'
 
 
 # In[ ]:
